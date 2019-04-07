e7fc8668877ce462a60e35107c60b1eb2c296a67
Eli Garcia <eligarcia@Elis-MacBook-Pro.local>
2019-04-07 13:25:19 -0400

    ReallyBigDog is done

--- check_brackets.c
+++ check_brackets.c
@@ -4,29 +4,70 @@
 int main() {
     int max_line = 105000;
 	char input_buffer[max_line];
-	int input_len = 0;	
-	
-	fgets(input_buffer, max_line, stdin);	
+	int input_len = 0;
+
+	fgets(input_buffer, max_line, stdin);
 	input_buffer [strcspn (input_buffer, "\r\n")] = '\0';  //remove end-of-line characters
 
     input_len = strlen(input_buffer);
     printf("%d characters were read.\n",input_len);
     // printf("The input was: '%s'\n",input_buffer);
 	fflush(stdout);
-    Stack * opening_brackets_stack = createStack(max_line); 
+    Stack * opening_brackets_stack = createStack(max_line);
     for (int position = 0; position < input_len; ++position) {
         char next = input_buffer[position];
 
         if (next == '(' || next == '[' || next == '{') {
+            Bracket item;
             // Process opening bracket, write your code here
+            if (next == '(') {
+                item.type = ROUND;
+            }
+            else if (next == '[') {
+                item.type = SQUARE;
+            }
+            else {
+                item.type = CURLY;
+            }
+            item.position = position;
+            push(opening_brackets_stack, item);
         }
 
         if (next == ')' || next == ']' || next == '}') {
             // Process closing bracket, write your code here
+            if (isEmpty(opening_brackets_stack)) {
+                printf("%d\n", position + 1);
+                return 0;
+            }
+            
+            Bracket item;
+            if (next == ')') {
+                item.type = ROUND;
+            }
+            else if (next == ']') {
+                item.type = SQUARE;
+            }
+            else {
+                item.type = CURLY;
+            }
+
+            Bracket top = pop(opening_brackets_stack);
+            if (item.type != top.type) {
+                printf("%d\n", position + 1);
+                return 0;
+            }
         }
     }
 
     // Printing answer, write your code here
-	printf("My result is:\n");
+    Bracket bottom;
+    if (!isEmpty(opening_brackets_stack)) {
+        while (!isEmpty(opening_brackets_stack)) {
+            bottom = pop(opening_brackets_stack);
+        }
+        printf("%d\n", bottom.position + 1);
+        return 0;
+    }
+	printf("Success\n");
     return 0;
 }
--- stack.h
+++ stack.h
@@ -13,30 +13,31 @@ There are two ways to implement a stack:
 
 //We are interested in brackets of 3 types: (), [], {}
 enum BracketType {ROUND=0, SQUARE=1, CURLY=2};
- 
+
 //Our items are of type Bracket
-typedef struct { 
+typedef struct {
     enum BracketType type;
     int position;
 }Bracket;
 
 // A structure to represent a stack node for a linked list
-typedef struct StackNode 
-{ 
-    Bracket data; 
-    struct StackNode* next; 
-}StackNode; 
+typedef struct StackNode
+{
+    Bracket data;
+    struct StackNode* next;
+}StackNode;
 
-// A structure to represent a stack 
-typedef struct  {       
+// A structure to represent a stack
+typedef struct  {
     unsigned capacity;
-	int top;	
+	int top;
     //TODO: depending on the implementation - fill-in the fields of a struct
-}Stack; 
+    Bracket* items;
+}Stack;
 
 
 
-// function to create a stack of given capacity. 
+// function to create a stack of given capacity.
 Stack* createStack(unsigned capacity);
 
 // Stack is full when top index is equal to capacity-1
@@ -45,9 +46,9 @@ int isFull(Stack* stack);
 // Stack is empty when top is equal to -1 (or head of the list is NULL)
 int isEmpty(Stack* stack);
 
-// Function to add an item to stack.  
+// Function to add an item to stack.
 void push(Stack* stack, Bracket item);
 
-// Function to remove an item from stack and return it. 
-Bracket pop(Stack* stack);     
+// Function to remove an item from stack and return it.
+Bracket pop(Stack* stack);
 #endif
